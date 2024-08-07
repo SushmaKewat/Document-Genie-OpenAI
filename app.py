1f6c42b64518cf0824dd61dcf6286d440da23f9d
Sushma <sushmak1693@gmail.com>
2024-08-07 12:33:08 +0530

    added utility functions to utils.py

--- app.py
+++ app.py
@@ -1,93 +1,14 @@
 import streamlit as st
-
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-#from langchain_community.llms import OpenAI
-
-from langchain_community.vectorstores.faiss import FAISS
-from langchain_community.document_loaders.pdf import PyPDFLoader
-from langchain_community.chat_message_histories import StreamlitChatMessageHistory
-from langchain.memory.buffer import ConversationBufferMemory
-
-from langchain.chains.qa_with_sources.loading import load_qa_with_sources_chain
-from langchain_core.prompts.prompt import PromptTemplate
-
 import os
-import shutil
-from dotenv import load_dotenv
-from itertools import groupby
 
-load_dotenv()
-# Dummy credentials
-USERNAME = os.getenv("APP_USERNAME")
-PASSWORD = os.getenv("PASSWORD")
+from langchain_community.document_loaders.pdf import PyPDFLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores.faiss import FAISS
 
-st.set_page_config(page_title="Doc Genie",page_icon="📑", layout="wide")
+from utils import remove_files, login, get_conversational_chain, get_vector_store, get_text_chunks
 
-def get_pdf_text(pdf_docs):
-    text = ""
-    for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-    return text
-
-def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-    chunks = text_splitter.split_documents(text)
-    ids = get_document_splits_with_ids(chunks)
-    return chunks, ids
-
-def get_document_splits_with_ids(doc_chunks):
-    document_ids = []
-    
-    for page, chunks in groupby(doc_chunks, lambda chunk : chunk.metadata['page']):
-        document_ids.extend([f"Source: {chunk.metadata['source'].split('/')[-1]}, Page no.: {page+1}, Chunk ID: {chunk_id}" for chunk_id, chunk in enumerate(chunks)])
-    
-    return document_ids
-
-def get_vector_store(text_chunks, ids, api_key):
-    try:
-        embeddings = OpenAIEmbeddings(openai_api_key=api_key)
-        vector_store = FAISS.from_documents(documents=text_chunks, ids=ids, embedding=embeddings)
-        
-        vector_store.save_local("faiss_index")
-        
-    except Exception as e:
-        print(f'[Vector Store Error]: Could not create the vector store.')
-
-def get_conversational_chain(api_key):
-    prompt_template = """
-    You are a firendly PDF assistant the helps the user to understand the contents of the uploaded files and give responses based on the content present in the context.
-    For the first query, greet the user.
-    Answer the question/query correctly from the provided context, make sure to provide all the details. 
-    If the user asks for the summary of the document, go through the contents of the document and give your response. 
-    If you don't find the answer, go through the context again, even then if the answer is not in provided context just say, "Answer is not available in the context", don't provide the wrong answer and do not leave any answer unfinished. Always provide full answer and complete the sentence.
-    \n\n
-    =====BEGIN DOCUMENT=====
-    {summaries}
-    =====END DOCUMENT=====
-
-    =====BEGIN CONVERSATION=====
-    {conversation_memory}
-    Question: \n{question}\n
-
-    Answer:
-    """
-
-    history = StreamlitChatMessageHistory(key='chat_messages') # retrieve the history of the streamlit application
-    memory = ConversationBufferMemory(chat_memory = history, input_key='question', memory_key='conversation_memory') # store the history in the memory
-#Context:\n {context}\n
-    # iterate over the history
-    for msg in history.messages:
-        st.chat_message(msg.type).write(msg.content)
-
-    model = ChatOpenAI(temperature=0.5, openai_api_key=api_key)
-    prompt = PromptTemplate(template=prompt_template, input_variables=["conversation_memory", "question", "summaries"])
-    chain = load_qa_with_sources_chain(llm=model, chain_type="stuff", memory=memory, prompt=prompt)
-  
-    return chain
+st.set_page_config(page_title="Doc Genie",page_icon="📑", layout="wide")   
+ 
 
 def user_input(user_question, api_key):
     embeddings = OpenAIEmbeddings(openai_api_key=api_key)
@@ -106,20 +27,15 @@ def user_input(user_question, api_key):
     source = f'Page: {metadata[0]}, Source: {metadata[1]}'
     
     st.chat_message("ai").markdown(f"{response['output_text']}\n\n :gray[{source}]")
-    
 
-def login(username, password):
-    return username == USERNAME and password == PASSWORD  
-
-spacer_left, form, spacer_right = st.columns([1, 1, 1])
+spacer_left, form, spacer_right = st.columns([0.5, 2, 0.5])
 
 def main():
     if "logged_in" not in st.session_state:
         st.session_state.logged_in = False
 
     if st.session_state.logged_in:
-        st.header("RAG Chatbot💁")
-        st.markdown(""" ## Document Genie: Get instant insights from your Documents""")
+        st.markdown(""" ## Document Genie 💁: Get instant insights from your Documents""")
         
         # This is the first API key input; no need to repeat it in the main function.
         api_key = st.text_input("Enter your OpenAI API Key:", type="password", key="api_key_input")
@@ -133,7 +49,7 @@ def main():
 
                     2. **Upload Your Documents**: The system accepts multiple PDF files at once, analyzing the content to provide comprehensive insights.
 
-                    3. **Ask a Question**: After processing the documents, ask any question related to the content of your uploaded documents for a precise answer.""")
+                    3. **Ask a Question**: After processing the documents, you can ask any question related to the content of your uploaded documents.""")
         
         docs = []
         if not os.path.exists("docs"):
@@ -141,11 +57,10 @@ def main():
             
         with st.sidebar:
             st.title("Menu")
-            pdf_docs = st.file_uploader("Upload your PDF files and click the Submit Button", type=".pdf", accept_multiple_files=True, key="pdf_uploader")
-            if st.button("Submit", key="process_button") and api_key:  # Check if API key is provided before processing
+            pdf_docs = st.file_uploader("Upload your PDF files and click the Submit button", type=".pdf", accept_multiple_files=True, key="pdf_uploader")
+            if st.button("Submit", use_container_width=True, key="process_button") and api_key:  # Check if API key is provided before processing
                 with st.spinner("Processing..."):
                     for pdf in pdf_docs:
-                        #raw_text = get_pdf_text(pdf_docs)
                         filepath = os.path.join("docs/", pdf.name)
                         os.makedirs(os.path.dirname(filepath), exist_ok=True)
                         with open(filepath, "wb") as f:
@@ -158,23 +73,25 @@ def main():
                         
                         st.success(f"{pdf.name} embedded successfully.")
                     st.caption("You can now ask questions from the uploaded documents.")
-           
-            if st.button("Logout", key="logout_button"):
-                    shutil.rmtree("faiss_index", ignore_errors=True)
-                    shutil.rmtree("docs", ignore_errors=True)
+            
+            col1, col2 = st.columns(2)
+            with col1:
+                if st.button("Clear Chat", use_container_width=True):
+                    st.session_state.chat_messages = []
+                    st.rerun()
+            
+            with col2:       
+                if st.button("Logout", use_container_width=True, key="logout_button"):
+                    remove_files()
                     st.session_state.logged_in = False
                     st.rerun()
     
-            if st.button("Clear Chat"):
-                    st.session_state.chat_messages = []
-                    st.rerun()
+            
                 
         if x:=st.chat_input("Ask a Question from the PDF Files", key='user_question'): # input for continuous conversation
        
             if x and api_key:  # Ensure API key and x, that is,user question are provided
-                 user_input(x, api_key) # this function is called every time a user enters a query
-
-            
+                 user_input(x, api_key) # this function is called every time a user enters a query 
 
     else:
         with form:
